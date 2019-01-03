f16fca19aaece7705ea033bfd177645c5775c70c
Dan Tenenbaum <dtenenba@fhcrc.org>
2019-01-03 09:54:12 -0800

    expand to show koshu

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ import timeout_decorator
 PARTITIONS_PER_ROW = 3
 ITEMS_TO_SHOW = 25
 
+
 def run_ssh_command(command):
     """
     Run an ssh command. The 'ssh' executable is not required
@@ -29,17 +30,17 @@ def run_ssh_command(command):
     # This key restricts users to squeue and sinfo commands,
     # see dashboard_wrapper.sh.
     key_file = 'slurmdashboard_rsa'
-    host = "rhino1" # don't let DNS round-robin until rhino3 is working again
+    host = "rhino1"  # don't let DNS round-robin until rhino3 is working again
 
     ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # how bad is this?
+    ssh.set_missing_host_key_policy(
+        paramiko.AutoAddPolicy())  # how bad is this?
 
     ssh.connect(host, key_filename=key_file, username=username)
     _, stdout, _ = ssh.exec_command(command)
     # TODO check for errors?
     ret = "".join(stdout.readlines())
     return ret
-
 
 
 def get_local_ip():
@@ -49,6 +50,7 @@ def get_local_ip():
     local_ip = sock.getsockname()[0]
     sock.close()
     return local_ip
+
 
 def is_at_home():
     """Is dan at home without hutch network access?"""
@@ -64,6 +66,7 @@ def get_data_for_partition(data, partition):
     data = data[data['PARTITION'] == partition]
     data = data[~data['JOBID'].isin(ids_to_remove)]
     return data
+
 
 def get_partitions(data):
     """Get partitions from data"""
@@ -85,10 +88,10 @@ def get_stats_for_data(data, nodes, partition):
         partition = "campus*"
     nodes = nodes[nodes['PARTITION'] == partition]
 
-
     return dict(total=int(nodes.sum()["CPUS"]),
                 running=int(data[data['ST'] == 'R'].sum()["CPUS"]),
                 pending=int(data[data['ST'] == 'PD'].sum()["CPUS"]))
+
 
 @timeout_decorator.timeout(10, use_signals=False)
 def get_data(cluster, featurefilter='', partitionfilter=''):
@@ -104,8 +107,10 @@ def get_data(cluster, featurefilter='', partitionfilter=''):
         squeue_fh = open("squeue.txt")
         sinfo_fh = open("sinfo.txt")
     else:
-        squeue = run_ssh_command(squeuecmd).replace("CLUSTER: beagle\n", "").replace("CLUSTER: gizmo\n", "").strip().replace('"', "")
-        sinfo = run_ssh_command(sinfocmd).replace("CLUSTER: beagle\n", "").replace("CLUSTER: gizmo\n", "").strip().replace('"', "")
+        squeue = run_ssh_command(squeuecmd).replace("CLUSTER: beagle\n", "").replace(
+            "CLUSTER: gizmo\n", "").strip().replace('"', "").replace("CLUSTER: koshu\n", "")
+        sinfo = run_ssh_command(sinfocmd).replace("CLUSTER: beagle\n", "").replace("CLUSTER: koshu\n", "").replace(
+            "CLUSTER: gizmo\n", "").strip().replace('"', "")
 
         squeue_fh = StringIO(squeue)
         sinfo_fh = StringIO(sinfo)
@@ -116,34 +121,44 @@ def get_data(cluster, featurefilter='', partitionfilter=''):
     squeue_fh.close()
     sinfo_fh.close()
 
-
     if featurefilter != '':
         nodes = nodes[(nodes['FEATURES'].str.contains(featurefilter))]
 
     df0 = jobs.sort_values("CPUS", 0, False)
     return df0, nodes
 
-app = flask.Flask(__name__) # pylint: disable=invalid-name
+app = flask.Flask(__name__)  # pylint: disable=invalid-name
+
 
 def rowstart(idx):
     """Determine if we are at the beginning of a row."""
     _, remainder = divmod(idx, PARTITIONS_PER_ROW)
     return remainder == 0
 
+
 def rowend(idx):
     """Determine if we are at the end of a row."""
     _, remainder = divmod(idx, PARTITIONS_PER_ROW)
     return remainder == (PARTITIONS_PER_ROW - 1)
+
 
 @app.route("/")
 def default_route():
     """Default flask route (/)"""
     return get_cluster_status()
 
+
 @app.route("/beagle")
 def beagle_route():
     """Route for beagle info"""
     return get_cluster_status("beagle")
+
+
+@app.route("/koshu")
+def koshu_route():
+    """Route for koshu info"""
+    return get_cluster_status("koshu")
+
 
 def get_cluster_status(cluster=""):
     """Route for /, display slurm usage data as web page."""
